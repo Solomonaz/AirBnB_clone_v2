8754bef65e9f0563977cc486a6324fd8492870ef
Solomon <solomonaz2022@gmail.com>
2023-04-24 01:25:07 -0700

    Air bnb clone

--- models/state.py
+++ models/state.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-""" State Module for HBNB project for AirBNB_clone_v2"""
-
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.engine.file_storage import FileStorage
-from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class definition in the next line"""
+    """ State class / table model"""
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade="all, delete")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
 
-    @property
-    def cities(self):
-        """Get a list of all related City objects."""
-        city_list = []
-        for city in list(models.storage.all(City).values()):
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+        @property
+        def cities(self):
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
